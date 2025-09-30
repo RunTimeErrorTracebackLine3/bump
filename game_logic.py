ed77a29953a748ed9b521ff00bc110deaa3c6085
Joseph Sasin <jsasin1990@gmail.com>
2025-09-29 20:24:22 -0700

    added beat

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,6 @@
 moves = ["bos-g", "scorpion", "block", "poke", "shark_attack", "tsunami", "snake", "mirror", "wait"]
 
+# whatevers in the right list is what the left thing beats
 win_moves = {
     "bos-g": ["scorpion", "spartan", "shark_attack", "tsunami", "wait", "mirror"],
     "scorpion": ["block", "wait", "tsunami"],
@@ -11,3 +12,14 @@ win_moves = {
     "mirror": ["scorpion", "shark_attack", "tsunami"],
     "wait": [""]
 }
+
+def beats(m1: str, m2: str):
+    # 1 = m1 wins, 0 = draw, -1, = m2 wins
+    if m1 == m2:
+        return 0
+    elif m2 in win_moves.get(m1, []):
+        return 1
+    elif m1 in win_moves.get(m2, []):
+        return -1
+    else:
+        return 0
